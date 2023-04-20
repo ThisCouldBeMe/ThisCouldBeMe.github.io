02fbdfa088d8cbedbf15a0dac7748af8b41cd7e0
ThisCouldBeMe <73258654+ThisCouldBeMe@users.noreply.github.com>
2023-04-20 16:27:02 +0200

    Add files via upload

---
+++ Files/js/Index.js
@@ -0,0 +1,15 @@
+const button = document.querySelector(".upload");
+const image = document.querySelector(".logo");
+const taskButton = document.querySelector(".taskbutton");
+
+button.addEventListener("click", () => {
+    const apiKey = "AcVfazSkJQCiFlLIvAWqAz";
+    const client = filestack.init(apiKey);
+    const options = {
+        onUploadDone: (uploadResponse) => {
+            console.log("onUploadDone", uploadResponse);
+            image.setAttribute("src", uploadResponse.filesUploaded[0].url);
+        },
+    };
+    client.picker(options).open();
+});
